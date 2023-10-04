e0cbb7329499a569c979ec927cc5d9667fbeea7a
Marius Kaufman <marius.kaufman@web.de>
2023-10-04 20:09:10 +0200

    -Add seetings to enable/disable swagger / swagger-ui -Remove test controller

---
+++ Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using WebSchoolPlanner.Swagger;
+
+namespace WebSchoolPlanner.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        return services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigureOptions>()
+            .AddSwaggerGen();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using WebSchoolPlanner.Swagger;
+using WebSchoolPlanner.Extensions;
 
 namespace WebSchoolPlanner;
 
 public class Startup
 {
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
@@ -18,8 +25,8 @@ public class Startup
             options.SubstituteApiVersionInUrl = true;
         });
 
-        services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigureOptions>();
-        services.AddSwaggerGen();
+        if (_configuration["Swagger:Use"] == true.ToString())
+            services.AddSwagger();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,17 +35,23 @@ public class Startup
         app.UseEndpoints(options => options.MapControllers());
 
         app.UseApiVersioning();
-        app.UseSwagger();
-        app.UseSwaggerUI(options =>
+
+        if (_configuration["Swagger:Use"] == true.ToString())
+            app.UseSwagger();
+
+        if (_configuration["Swagger:UseUI"] == true.ToString())
         {
-            // Register all api versions
-            IApiVersionDescriptionProvider provider = app.ApplicationServices.GetService<IApiVersionDescriptionProvider>()!;
-            foreach (ApiVersionDescription description in provider.ApiVersionDescriptions)
+            app.UseSwaggerUI(options =>
             {
-                string url = $"/swagger/{description.GroupName}/swagger.json";
-                string name = description.GroupName.ToUpperInvariant();
-                options.SwaggerEndpoint(url, name);
-            }
-        });
+                // Register all api versions
+                IApiVersionDescriptionProvider provider = app.ApplicationServices.GetService<IApiVersionDescriptionProvider>()!;
+                foreach (ApiVersionDescription description in provider.ApiVersionDescriptions)
+                {
+                    string url = $"/swagger/{description.GroupName}/swagger.json";
+                    string name = description.GroupName.ToUpperInvariant();
+                    options.SwaggerEndpoint(url, name);
+                }
+            });
+        }
     }
 }
