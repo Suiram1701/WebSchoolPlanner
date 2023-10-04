3f05f4e001003110050e4511e771124cbc0634be
Marius Kaufman <marius.kaufman@web.de>
2023-10-04 18:26:11 +0200

    Add project files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace WebSchoolPlanner;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(builder =>
+            {
+                builder.UseStartup<Startup>();
+            })
+            .Build()
+            .Run();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace WebSchoolPlanner;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+    }
+}
